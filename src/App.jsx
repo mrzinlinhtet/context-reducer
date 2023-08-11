4383542b0c786f8e2273e7718ac7ff11d7abf36c
zinlinxtet <devzinlinhtet@gmail.com>
2023-08-11 11:40:19 +0630

    add context and reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import One from "./One"
-import Two from "./Two"
-
+import {useReducer} from 'react'
+import { useStateContext } from './context/StateContext'
 
 const App = () => {
+  // const initialState = {
+  //   count1: 0,
+  //   count2: 100
+  // }
 
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case 'increment':
+  //       return {...state, count1: state.count1 + action.payload}
+  //     case 'decrement':
+  //       return {...state, count1: state.count1 - action.payload}
+  //     case 'reset':
+  //       return initialState
+  //     case 'increment2':
+  //       return {...state, count2: state.count2 + action.payload}
+  //     case 'decrement2':
+  //       return {...state, count2: state.count2 - action.payload}
+  //     default:
+  //       return state
+  //   }
+  // }
+
+  // const [state, dispatch] = useReducer(reducer, initialState)
+
+  const {state, dispatch} = useStateContext()
 
   return (
     <div>
-      <One />
-      <Two />
+      <h1>{state}</h1>
+      <button onClick={() => dispatch({type:"decrement"})}>-</button>
+      <button onClick={() => dispatch({type:"increment"})}>+</button>
+      <button onClick={() => dispatch({type:"reset"})}>Reset</button>
+      {/* <h1>{state.count1}</h1>
+      <button onClick={() => dispatch({type: 'decrement', payload: 1})}>-</button>
+      <button onClick={() => dispatch({type: 'increment', payload: 1})}>+</button>
+      <button onClick={() => dispatch({type: 'reset'})}>reset</button> 
+      <hr />
+      <h1>{state.count2}</h1>
+      <button onClick={() => dispatch({type: 'decrement2', payload: 10})}>-</button>
+      <button onClick={() => dispatch({type: 'increment2', payload: 10})}>+</button> */}
+      {/* <button onClick={() => dispatch({type: 'reset'})}>reset</button>  */}
     </div>
   )
 }
--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -1,11 +1,30 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 export const StateContext = createContext();
 
 const StateContextProvider = ({ children }) => {
-    const [count, setCount]= useState(0)
-    const inc = () => setCount(count + 1)
-    const dec = () => setCount(count - 1)
-    const data = {count, inc, dec}
+
+  const initialState = 0
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'increment':
+        return state + 1
+      case 'decrement':
+        return state - 1
+      case 'reset':
+        return initialState
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+    // const [count, setCount]= useState(0)
+    // const inc = () => setCount(count + 1)
+    // const dec = () => setCount(count - 1)
+
+    const data = {state, dispatch}
   return <StateContext.Provider value={data}>{children}</StateContext.Provider>;
 };
 
